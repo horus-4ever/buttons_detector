fix bug in the computation of the giou

criterion.py
@@ -57,13 +57,12 @@ def compute_giou(box1: torch.Tensor, box2: torch.Tensor) -> torch.Tensor:
     y1 = torch.min(box1[..., 1] - box1[..., 3] / 2, box2[..., 1] - box2[..., 3] / 2)
     x2 = torch.max(box1[..., 0] + box1[..., 2] / 2, box2[..., 0] + box2[..., 2] / 2)
     y2 = torch.max(box1[..., 1] + box1[..., 3] / 2, box2[..., 1] + box2[..., 3] / 2)
-    # convert to a box
-    large_box = torch.stack([x1, y1, x2 - x1, y2 - y1], dim=-1)
     # now compute the area of the large box
-    area_large = (x2 - x1) * (y2 - y1)
+    area_large = ((x2 - x1) * (y2 - y1)).clamp(min=1e-6)  # add a small epsilon to avoid zero divisions
     # compute the IoU then the GIoU
     iou = compute_iou(box1, box2)
-    giou = iou - (area_large - compute_union(box1, box2))
+    union = compute_union(box1, box2)
+    giou = iou - (area_large - union) / area_large
     return giou
 
 
@@ -109,7 +108,7 @@ class HungarianMatcher(nn.Module):
         indices = []
 
         for b in range(bs):
-            tgt_labels = targets[b]["labels"]     # [num_gt] number of ground-truth buttons (2, 3, 4, 5, 6, 7)
+            tgt_labels = targets[b]["labels"]     # [num_gt] number of ground-truth buttons
             # print(tgt_labels)
             tgt_coords = targets[b]["buttons"]    # [num_gt, 4]
             # print(tgt_coords)