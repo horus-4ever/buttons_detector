fix GIoU computation bug in the Hungarian Matcher (used wrong tensors previously)

model/criterion.py
@@ -54,12 +54,10 @@ class HungarianMatcher(nn.Module):
 
         for b in range(bs):
             tgt_labels = targets[b]["labels"]     # [num_gt] number of ground-truth buttons (2, 3, 4, 5, 6, 7)
-            # print(tgt_labels)
-            tgt_coords = targets[b]["buttons"]    # [num_gt, 4]
-            tgt_holes = targets[b]["keypoints"]
-            # print(tgt_coords)
+            tgt_buttons = targets[b]["buttons"]    # [num_gt, 4]
+            tgt_holes = targets[b]["keypoints"]   # [num_gt, 4]
 
-            if tgt_coords.numel() == 0:
+            if tgt_buttons.numel() == 0:
                 indices.append((
                     torch.empty(0, dtype=torch.int64),
                     torch.empty(0, dtype=torch.int64)
@@ -72,12 +70,20 @@ class HungarianMatcher(nn.Module):
             cost_class = -out_prob[b][:, tgt_labels]
 
             # Coordinate cost
-            # out_coord[b]: [Q, 4], tgt_coords: [num_gt, 4]
-            cost_coord = torch.cdist(out_coord[b], tgt_coords, p=1)
+            # out_coord[b]: [Q, 4], tgt_buttons: [num_gt, 4]
+            cost_coord = torch.cdist(out_coord[b], tgt_buttons, p=1)
             cost_hole = torch.cdist(pred_holes[b], tgt_holes, p=1)
-            # TODO: add a giou cost
-            cost_giou = -compute_giou(out_coord[b], pred_holes[b])
-            # Total cost
+            # compute the GIoU cost for each pair
+            giou_buttons = compute_giou(
+                pred_buttons[b][:, None, :], # [Q, 1, 4]
+                tgt_buttons[None, :, :], # [1, N, 4]
+            ) # [Q, N]
+            giou_holes = compute_giou(
+                pred_holes[b][:, None, :], # [Q, 1, 4]
+                tgt_holes[None, :, :], # [1, N, 4]
+            ) # [Q, N]
+            cost_giou = -(giou_buttons + giou_holes)
+            # total cost
             C = self.cost_class * cost_class + self.cost_coord * (cost_coord + cost_hole) + cost_giou
             C = C.cpu()
 

model/prtr.py
@@ -78,14 +78,13 @@ class PRTR(nn.Module):
         for feature_map in feature_maps:
             # feature_map: [B, embed_dim, Hl, Wl]
             _, _, H, W = feature_map.size()
-            height_indices = torch.tensor(torch.round(torch.linspace(0.5 / H, (1.0 - 0.5 / H), H) * H_img), dtype=torch.int, device=feature_map.device)
-            width_indices = torch.tensor(torch.round(torch.linspace(0.5 / W, 1.0 - 0.5 / W, W) * W_img), dtype=torch.int, device=feature_map.device)
-            h_indices, w_indices = torch.meshgrid(height_indices, width_indices, indexing='ij')
-            # [B, Hl, Wl]
-            level_map = masks[:, h_indices, w_indices]
-            # [B, Hl, Wl] -> [B, 1, Hl, Wl]
-            level_map = level_map[:, None, :, :]
-            output.append(level_map)
+            size = (H, W)
+            level_mask = F.interpolate(
+                masks[:, None].float(),
+                size=size,
+                mode="nearest",
+            ).to(torch.bool)
+            output.append(level_mask)
         return output
 
 