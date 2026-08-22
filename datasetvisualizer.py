813f9821108110bd18c8e08c021ca558008098ab
Théophile PICHARD <theophile.pichard@telecomnancy.eu>
2026-08-22 10:01:56 +0900

    add kmeans analysis of the dataset's fastener size

--- datasetvisualizer.py
+++ datasetvisualizer.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import json
 from dataformat.dataset import DatasetConfig
 from dataformat.dataformat import Annotation
+from sklearn.cluster import KMeans
+import numpy as np
 
 
 
@@ -54,6 +56,26 @@ def get_fastener_size_stats(annotations: list[Annotation], im_size: int = 512):
         }
     }
 
+def plot_fastener_kmeans(annotations: list[Annotation], im_size: int = 512):
+    widths = []
+    heights = []
+    for ann in annotations:
+        for pair in ann.cloth.pairs:
+            widths.append(pair.fastener.bbox.w)
+            heights.append(pair.fastener.bbox.h)
+    input = np.column_stack([np.array(widths), np.array(heights)])
+    
+    kmeans = KMeans(n_clusters=3, init="k-means++", n_init=10, random_state=42)
+    cluster_labels = kmeans.fit_predict(input)
+    centroids = kmeans.cluster_centers_
+    print(centroids * np.array([im_size, im_size]))
+    plt.figure(figsize=(8, 6))
+    # Plot data points colored by their cluster label
+    plt.scatter(
+        input[:, 0], input[:, 1], c=cluster_labels, s=50, cmap="viridis"
+    )
+    plt.show()
+
 
 def get_button_size_stats(annotations: list[Annotation], im_size: int = 512):
     widths = []
@@ -83,6 +105,7 @@ if __name__ == "__main__":
     dataset = DatasetConfig.open(config_path=dataset_path).load()
     # training dataset is representative of the dataset so let's use it
     train_dataset = dataset.train_annotations
+    plot_fastener_kmeans(train_dataset)
     plot_buttons_distribution(train_dataset)
     plot_fastener_size(train_dataset)
     fastener_stats = get_fastener_size_stats(train_dataset)
